48d90d41fb2e1c582b455cdebc92648bf5314c1a
TrBinhMinh <bminh9900@gmail.com>
2022-07-18 18:01:01 +0700

    :rocket: upload to lecture 106

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
-  return (
-    <div>
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (username, userage) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: username, age: userage, id: Math.random().toString() },
+      ];
+    });
+  };
 
-    </div>
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
 }
 
---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,48 @@
+import Card from "./Card";
+import Button from "./Button";
+import styles from "./ErrorModal.module.css";
+import React from "react";
+import ReactDom from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+
+      <main className={styles.content}>
+        <p>{props.message}</p>
+      </main>
+
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
+  );
+};
+
+export default ErrorModal;
