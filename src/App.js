9a9ac38f90af47c24ddd847f8b8d09a6f23e7e25
Binh Minh Tran <minh.tb@gif.vn>
2022-08-09 13:45:55 +0700

    :rocket: initialize redux project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import Counter from './components/Counter';
+
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const toggleCartHandler = () => {
-    setCartIsShown(!cartIsShown);
-  };
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onToggleCart={toggleCartHandler} />}
-      <Header onToggleCart={toggleCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <Counter />
   );
 }
 
