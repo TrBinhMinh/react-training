49f13f669070c059886a07e20a9301e527f42e4c
Binh Minh Tran <minh.tb@gif.vn>
2022-08-09 22:05:25 +0700

    :art: update to lesson 237

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
+import { createStore } from "redux";
+
 const redux = require("redux");
 
 const counterReducer = function (state = { counter: 0 }, action) {
-  return {
-    counter: state,
-  };
-};
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-const store = redux.createStore(counterReducer);
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+export const store = redux.createStore(counterReducer);
+
+// store.dispatch({ type: "increment" });
+// store.dispatch({ type: "decrement" });
+export default store;
