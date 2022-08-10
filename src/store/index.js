98222a8b895e7459c2fbfa5cf6ccbde24b2b5dd0
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-08-10 16:22:43 +0700

    :art: update to lesson 246

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,49 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const redux = require("redux");
+const initialCounterState = { counter: 0, showCounter: true };
 
-const counterReducer = function (state = { counter: 0 }, action) {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
+const initialAuthState = { isAuthenticated: false };
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
-  return state;
-};
-export const store = redux.createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
+});
 
 // store.dispatch({ type: "increment" });
 // store.dispatch({ type: "decrement" });
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
