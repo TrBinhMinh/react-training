a3fd0a9733e2e1b417d960e474f3f110ebc82ff2
Binh Minh Tran <minh.tb@gif.vn>
2022-08-25 21:35:47 +0700

    :art: update to lesson 276

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,14 @@
+import { useParams } from "react-router";
+import { Fragment } from "react";
+
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
