fbf2290d464299d46f779c2a0b61d2c88529d08e
Binh Minh Tran <minh.tb@gif.vn>
2022-08-29 22:33:56 +0700

    :art: update to lesson 283

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,24 @@
 import { useParams, Route } from "react-router-dom";
 import { Fragment } from "react";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Minh", text: "Learning React is fun!" },
+  { id: "q2", author: "Tran", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
