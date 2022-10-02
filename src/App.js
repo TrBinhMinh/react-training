067c342c2559ebfd9e53fa7278504c4798e02000
Binh Minh Tran <minh.tb@gif.vn>
2022-10-02 21:38:47 +0700

    :rocket: initialize animation project

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
-import React, { Suspense } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-// import AllQuotes from "./pages/AllQuotes";
-// import QuoteDetail from "./pages/QuoteDetail";
-// import NewQuote from "./pages/NewQuote";
-import Layout from "./components/layout/Layout";
-// import NotFound from "./pages/NotFound";
-import LoadingSpinner from "./components/UI/LoadingSpinner";
+import React, { Component } from "react";
 
-const NewQuote = React.lazy(() => import("./pages/NewQuote"));
-const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
-const NotFound = React.lazy(() => import("./pages/NotFound"));
-const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
 
-function App() {
-  return (
-    <Layout>
-      <Suspense
-        fallback={
-          <div className="centered">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <AllQuotes />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="/new-quote">
-            <NewQuote />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Suspense>
-    </Layout>
-  );
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        {this.state.modalIsOpen && (
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        )}
+        {this.state.modalIsOpen && <Backdrop show={this.state.modalIsOpen} />}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
 }
 
 export default App;
