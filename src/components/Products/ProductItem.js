1118fdd3d87f5107100258efd171d1eeda67fbe1
Minh <bminh9900@gmail.com>
2022-10-05 22:15:39 +0700

    :art: update to lesson 380

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,31 +1,33 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import Card from "../UI/Card";
+import { useStore } from "../../hooks-store/store";
 import "./ProductItem.css";
-import { ProductsContext } from "../../context/products-context";
 
-const ProductItem = (props) => {
-    const toggleFav = useContext(ProductsContext).toggleFav;
+const ProductItem = React.memo((props) => {
+    console.log('RENDERING');
+  const dispatch = useStore(false)[1];
 
-    const toggleFavHandler = () => {
-        // dispatch(toggleFav(props.id));
-        toggleFav(props.id);
-    };
+  const toggleFavHandler = () => {
+    // dispatch(toggleFav(props.id));
+    // toggleFav(props.id);
+    dispatch('TOGGLE_FAV', props.id)
+  };
 
-    return (
-        <Card style={{ marginBottom: "1rem" }}>
-            <div className="product-item">
-                <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
-                <p>{props.description}</p>
-                <button
-                    className={!props.isFav ? "button-outline" : ""}
-                    onClick={toggleFavHandler}
-                >
-                    {props.isFav ? "Un-Favorite" : "Favorite"}
-                </button>
-            </div>
-        </Card>
-    );
-};
+  return (
+    <Card style={{ marginBottom: "1rem" }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
+        <p>{props.description}</p>
+        <button
+          className={!props.isFav ? "button-outline" : ""}
+          onClick={toggleFavHandler}
+        >
+          {props.isFav ? "Un-Favorite" : "Favorite"}
+        </button>
+      </div>
+    </Card>
+  );
+});
 
 export default ProductItem;
--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -4,14 +4,35 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
-    const setState = useState(globalState)[1];
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
 
-    useEffect(() => {
-        listeners.push(setState);
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
 
-        return () => {
-            listeners = listeners.filter((li) => li !== setState);
-        };
-    }, [setState]);
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
