9e1e70e2097aa522aee49ad85eb0dc02f8c0d6ed
Minh <bminh9900@gmail.com>
2022-10-16 14:40:46 +0700

    :rocket: initialize typescript project

---
+++ basics.ts
@@ -0,0 +1,16 @@
+let age: number = 25;
+let username: string;
+let isDev: boolean = true;
+let hobbies: string[];
+type Person = {
+  name: string;
+  age: number;
+};
+let person: Person;
+let people: Person[];
+let course: string | number;
+
+// Functions & types
+function add(a: number, b: number) {
+  return a + b;
+}
